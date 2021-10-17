3a52c216430afe456cc89c808a246c7b889bde1a
jaeha <jaeha23@naver.com>
2021-10-17 18:49:14 +0900

    WithRouterSample

--- src/Profiles.js
+++ src/Profiles.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 
 import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
 
 function Profiles() {
   return (
@@ -18,6 +19,7 @@ function Profiles() {
 
       <Route path="/profiles" exact render={() => <div>유저를 선택해주세요.</div>} />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 }
