379b7cd301cd95df35995b6c3903ff44e5926903
jaeha <jaeha23@naver.com>
2021-10-17 18:24:54 +0900

    App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <hr />
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
